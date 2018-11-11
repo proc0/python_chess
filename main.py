f7656f9fd1eb2386c06495908050746d3d3302f5
proc0 <alanvx@gmail.com>
2018-11-11 13:57:19 -0800

    dev: adding dummy board configuration

--- main.py
+++ main.py
@@ -5,51 +5,66 @@ import pygame as pg
 from pygame import Surface
 from pprint import pprint
 from cursors import HAND_CURSOR, GRAB_CURSOR
+
 is_even = lambda n: n%2 == 0
 
 class Piece(pg.sprite.Sprite):
-  def __init__(self, props):
+  def __init__(self):
     pg.sprite.Sprite.__init__(self)
-    for k, v in props.items():
-      setattr(self, k, v)
-    self.surface = Surface(self.size, pg.SRCALPHA)
-
-  def draw(self):
+    # for k, v in props.items():
+    #   setattr(self, k, v)
     piece_path = os.path.join('images', 'bb3.png')
     piece_png = pg.image.load(piece_path)
-    self.surface.blit(piece_png, (0,0))
+    piece_rect = piece_png.get_rect()
+    self.piece_png = piece_png
+    self.surface = Surface((piece_rect[2],piece_rect[3]), pg.SRCALPHA)
+
+  def draw(self):
+    self.surface.blit(self.piece_png, (0,0))
 
 
 class Square:
+  piece = None
   surface = None
   def __init__(self, props):
     for k, v in props.items():
       setattr(self, k, v)
     self.surface = Surface((self.size, self.size))
     self.surface.fill(self.color)
-    self.piece = Piece({ 
-        'size': (self.size-30, self.size-30),
-        'bg_color': self.color
-      })
+    # self.piece = Piece()
 
   def draw(self):
     font = pg.font.SysFont('Arial', 10)
     coords = font.render(self.label, False, self.text_color)
-    self.piece.draw()
     self.surface.blit(coords, (self.pad/2, self.pad/2))
-    self.surface.blit(self.piece.surface, (self.pad, self.pad))
 
   def piece_hovering(self, coords):
-    in_x = coords[0] > self.x and coords[0] < self.x + self.size - self.pad
-    in_y = coords[1] > self.y and coords[1] < self.y + self.size - self.pad
+    in_x = coords[0] > self.x + self.pad and coords[0] < self.x + self.size - self.pad
+    in_y = coords[1] > self.y + self.pad and coords[1] < self.y + self.size - self.pad
     return self.piece and in_x and in_y
+
+  def place_piece(self, piece):
+    self.piece = piece
+    self.piece.draw()
+    self.surface.blit(self.piece.surface, (self.pad, self.pad))
+    return piece
 
 class Board:
   bg_color = (255,255,255)
   light = (255,255,255)
   dark = (50,50,50)
   squares = []
-
+  pieces = []
+  dummy_board = [
+      [0,0,1,0,0,1,0,1],
+      [0,0,1,0,0,1,0,0],
+      [0,1,0,0,0,1,0,0],
+      [0,0,0,0,1,1,0,0],
+      [0,0,1,0,0,0,0,0],
+      [0,1,1,0,0,1,0,0],
+      [1,0,1,0,0,1,0,0],
+      [0,1,0,0,0,1,0,0]
+    ]
   def __init__(self, size):
     self.size = (size[1], size[1])
     self.surface = Surface((self.size[0], self.size[1]))
@@ -68,9 +83,11 @@ class Board:
         alter = even_col if even_row else not even_col
         sq = Square({
           'size': sq_size,
+          '_x' : x-1,
+          '_y' : y-1,
           'x': x*sq_size - sq_size, 
           'y': y*sq_size - sq_size,
-          'pad': sq_size-10
+          'pad': 12,
           'color': self.dark if alter else self.light,
           'text_color': self.light if alter else self.dark,
           'label': str(chr(73-y)) + str(x)
@@ -79,7 +96,7 @@ class Board:
         row.append(sq)
 
       self.squares.append(row)
-      self.surface.blits(list(map(lambda s: (s.surface, (s.x, s.y)), row)))
+      self.surface.blits(list(map(lambda s: (s.place_piece(Piece()) if self.dummy_board[s._y][s._x] == 1 else True) and (s.surface, (s.x, s.y)), row)))
 
   def get_square(self, coords):
     sq_size = int(self.size[1]/8)
@@ -124,27 +141,30 @@ class Game():
       for event in pg.event.get():
         if event.type == pg.QUIT:
           run = False
-        elif event.type == pg.MOUSEBUTTONUP:
-          if(event.button == 1):
-              pg.mouse.set_cursor(*HAND_CURSOR)
-        elif event.type == pg.MOUSEBUTTONDOWN:
-          if(event.button == 1):
+
+        elif event.type == pg.MOUSEBUTTONDOWN or \
+          event.type == pg.MOUSEBUTTONUP:
+          if(self.is_move(board, event.pos)):
+            sq = board.get_square(event.pos)
+            if(event.button == 1 and sq.piece_hovering(event.pos)):
               pg.mouse.set_cursor(*GRAB_CURSOR)
+            else:
+              pg.mouse.set_cursor(*DEFAULT_CURSOR)
+
         elif event.type == pg.MOUSEMOTION:
-          click = event.buttons[0] == 1
-          pos = event.pos
-          if(self.is_move(board, pos)):
-            sq = board.get_square(pos)
-            if(click):
-              pg.mouse.set_cursor(*GRAB_CURSOR)
-            elif(sq.piece_hovering(pos)):
+          # board moves
+          if(self.is_move(board, event.pos)):
+            sq = board.get_square(event.pos)
+            if(sq.piece_hovering(event.pos)):
               pg.mouse.set_cursor(*HAND_CURSOR)
+              if(event.buttons[0] == 1):
+                pg.mouse.set_cursor(*GRAB_CURSOR)
             else:
               pg.mouse.set_cursor(*DEFAULT_CURSOR)
 
             players[0].move({ 
                 'board_click': True, 
-                'pos': pos 
+                'pos': event.pos 
               })
           else:
             pg.mouse.set_cursor(*DEFAULT_CURSOR)
