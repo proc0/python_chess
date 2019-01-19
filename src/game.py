f27bd946ba9c09f4cd7a08b649e47439dda548a9
proc0 <alanvx@gmail.com>
2019-01-18 17:57:52 -0800

    dev: refactoring game draw to use actions more abstractly

--- src/game.py
+++ src/game.py
@@ -27,57 +27,71 @@ class Game():
       board.draw()
       self.display.blit(board.surface, board.surface.get_rect())
 
-    if(self.action[0] == 'MOVING'):
-      evt = self.action[1][0]
-      sq = self.action[1][1]
-      
-      if(self.player.piece):
-        self.player.piece = move_piece(evt.pos, self.player.piece)
-      else:
+    refresh = False
+    if(self.action[0] == 'GRAB'):
+      evt = self.action[1]
+      sq = board.get_sq(evt.pos)
+      print(sq.piece)
+
+      if(sq.is_focused(evt.pos) and not self.player.piece):
         self.player.piece = move_piece(evt.pos, sq.remove_piece())
-      
-      sq.draw()
-      board.surface.blit(sq.surface, (sq.x, sq.y))
-      self.display.blit(board.surface, board.surface.get_rect())
-      self.display.blit(self.player.piece.surface, (self.player.piece.x,  self.player.piece.y))
-    
+        self.action = ('MOVING', evt)
+      else:
+        self.action = ('IDLE', evt)
+
+      pg.mouse.set_cursor(*GRAB_CURSOR)
+      refresh = True
+
+    if(self.action[0] == 'MOVING'):
+      evt = self.action[1]
+      sq = board.get_sq(evt.pos)
+
+      if (sq.is_focused(evt.pos) and not self.player.piece):
+        self.action = ('IDLE', evt)
+      elif(self.player.piece):
+        self.player.piece = move_piece(evt.pos, self.player.piece)
+
+      refresh = True
+
     if(self.action[0] == 'DROP'):
-      # evt = self.action[1][0]
-      sq = self.action[1][1]
+      evt = self.action[1]
+      sq = board.get_sq(evt.pos)
       sq.place_piece(self.player.piece)
       board.drop_piece(self.player.piece)
       self.player.piece = None
       self.player.move(sq)
-      self.display.blit(board.surface, board.surface.get_rect())
+      pg.mouse.set_cursor(*HAND_CURSOR)
+      self.action = ('IDLE', evt)
+      refresh = True
+
+    if(self.action[0] == 'IDLE'):
+      evt = self.action[1]
+      sq = board.get_sq(evt.pos) if evt else None
+      cursor = self.cursor
+      if (sq and sq.is_focused(evt.pos) and not self.player.piece):
+        cursor = HAND_CURSOR
+      pg.mouse.set_cursor(*self.cursor)
     
-    if(self.action[0] == 'DROP'):
-      self.action = ('IDLE', None)
+    if(refresh):
+      sq.draw()
+      board.surface.blit(sq.surface, (sq.x, sq.y))
+      self.display.blit(board.surface, board.surface.get_rect())
+      if(self.player.piece):
+        self.display.blit(self.player.piece.surface, (self.player.piece.x,  self.player.piece.y))
 
   def MouseButtonUp(self, event, board):
     if(self.player.piece):
-      pg.mouse.set_cursor(*HAND_CURSOR)
-      self.action = ('DROP', (event, board.get_sq(event.pos)))
+      self.action = ('DROP', event)
 
   def MouseButtonDown(self, event, board):
-    sq = board.get_sq(event.pos)
     is_leftclick = event.button == 1
-    if(is_leftclick and sq.is_focused(event.pos)):
-      self.action = ('MOVING', (event, sq))
-      pg.mouse.set_cursor(*GRAB_CURSOR)
-
+    if(is_leftclick):
+      self.action = ('GRAB', event)
 
   def MouseMotion(self, event, board):
-    sq = board.get_sq(event.pos)
     is_leftclick = event.buttons[0] == 1
-
     if(is_leftclick and self.player.piece):        
-      self.action = ('MOVING', (event, sq))
-      # self.player.piece = move_piece(event.pos, self.player.piece)
-    else:
-      cursor = self.cursor
-      if sq.is_focused(event.pos):
-        cursor = HAND_CURSOR
-      pg.mouse.set_cursor(*cursor)
+      self.action = ('MOVING', event)
 
   def run(self, board, players):
     self.player = players[0]
@@ -86,9 +100,10 @@ class Game():
     while not quit:
       for event in pg.event.get():
         quit = event.type == pg.QUIT
-        if pg.event.event_name(event.type) in self.pg_events:
+        pg_event = pg.event.event_name(event.type)
+        if pg_event in self.pg_events:
           if(board.has(event.pos)):
-            handle = getattr(self, pg.event.event_name(event.type))
+            handle = getattr(self, pg_event)
             handle(event, board)
       self.draw(board)
       pg.display.flip()
